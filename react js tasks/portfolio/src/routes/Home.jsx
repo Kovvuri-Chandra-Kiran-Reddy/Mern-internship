ef7b40883f95eefa4545dba7bb45c86e33ca83e0
Kovvuri-Chandra-Kiran-Reddy <chandrakiranreddykovvuri@gmail.com>
2023-06-14 19:54:18 +0530

    modified

--- react js tasks/portfolio/src/routes/Home.jsx
+++ react js tasks/portfolio/src/routes/Home.jsx
@@ -3,12 +3,15 @@ import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
 import Work from '../components/Work/Work'
 import MainHero from './../components/MainHero/MainHero'
+import Skills from './../components/Skills/Skills'
+
 const Home = () => {
   return (
     <div>
       <Navbar/>
       {/* <Hero/> */}
       <MainHero />
+      <Skills />
       <Work/>
       <Footer/>
     </div>
